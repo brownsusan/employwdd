c26efd97e6e5d0cd9ca1ced1e4af0125c82b42c8
Susan Brown <brownsusan@fullsail.edu>
2014-02-21 16:49:47 -0500

    main view responds to user logging in

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -4,17 +4,21 @@ angular.module('employwddApp').controller('MainCtrl', function($scope) {
 
 });
 
+
 angular.module('employwddApp').controller('PostCtrl', function($scope) {
 
 });
+
 
 angular.module('employwddApp').controller('PostsCtrl', function($scope) {
 	$scope.pageHeading = 'All Posts';
 });
 
+
 angular.module('employwddApp').controller('PostsPositionsCtrl', function($scope) {
 	$scope.pageHeading = 'Open Positions';
 });
+
 
 angular.module('employwddApp').controller('PostsFreelanceCtrl', function($scope) {
 	$scope.pageHeading = 'Freelance Opportunities';
